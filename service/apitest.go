0d00594a7571aa3fdf816d36c64cea11b62a746f
zanhaofang <2390839686@qq.com>
2018-01-22 17:07:37 +0800

    Create apitest.go

---
+++ service/apitest.go
@@ -0,0 +1,17 @@
+package service
+
+import (
+	"net/http"
+
+	"github.com/unrolled/render"
+)
+
+func apiTestHandler(formatter *render.Render) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, req *http.Request) {
+		formatter.JSON(w, http.StatusOK, struct {
+			ID      string `json:"id"`
+			Content string `json:"content"`
+		}{ID: "14331063", Content: "Hello from Go!"})
+	}
+}
