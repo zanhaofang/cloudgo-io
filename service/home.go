c1e34a6c019ed597bc10983ebef7c290ea7e9d86
zanhaofang <2390839686@qq.com>
2018-01-22 17:02:56 +0800

    Create home.go

---
+++ service/home.go
@@ -0,0 +1,14 @@
+package service
+
+import (
+	"net/http"
+
+	"github.com/unrolled/render"
+)
+
+func homeHandler(formatter *render.Render) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, req *http.Request) {
+		formatter.HTML(w, http.StatusOK, "index1", struct{}{})
+	}
+}
